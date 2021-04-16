5c033cc242d36f2ddab8ec49778c6bf088b1a5d7
Sai <saisandhya187@gmail.com>
2021-04-16 17:17:33 +0530

    Integrate onclick functions with buttons

--- js/favourites.js
+++ js/favourites.js
@@ -1,13 +1,28 @@
+let URLparams = (new URL(document.location)).searchParams;
+let id = URLparams.get('id');
+
+function favouriteClicked(){
+    if(checkFavourite()){
+        removeFromFavourites();
+    }
+    else{
+        addToFavourite();
+    }
+}
+
 //add to favourite function
-function addToFavourite(id){
+function addToFavourite(){
     let existing=localStorage.getItem('favourite');
     existing=existing?existing.split(','):[];
-    existing.push(id);
+    if(!checkFavourite(id)){
+      existing.push(id);
+    }
     localStorage.setItem('favourite', existing.toString());
+    showColor();
 }
 
 //check if a movie is in favourite list
-function checkFavourite(id){
+function checkFavourite(){
     let list=localStorage.getItem('favourite');
     list=list?list.split(','):false;
     if(!list){
@@ -31,13 +46,24 @@ function getFavourites(){
 }
 
 //remove a movie from favourites
-function removeFromFavourites(id){
+function removeFromFavourites(){
     let list=localStorage.getItem('favourite');
     list = list ? list.split(',') : [];
     if(list.length!==0){
         list = list.filter(item => item !== id)
     }
     localStorage.setItem('favourite', list.toString());
+    showColor();
 }
 
-// addToFavourite("tt1345836");
+
+function showColor(){
+  if(checkFavourite()){
+    document.getElementById('favourite').setAttribute('fill','red');
+  }
+  else{
+  document.getElementById('favourite').setAttribute('fill','currentColor');
+  }
+}
+
+showColor();
